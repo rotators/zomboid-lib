0c9a0de0fd121518ec76d976d830340d5bd57639
Wipe <wipe.git@gmail.com>
2022-02-03 16:09:09 +0100

    tweak

--- Contents/mods/RotatorsLib/media/lua/client/rLib.ClientAuto.lua
+++ Contents/mods/RotatorsLib/media/lua/client/rLib.ClientAuto.lua
@@ -4,6 +4,8 @@ require "rLib.Client"
 
 rLib.ClientAuto = {}
 
+local ISVM_FillMenuOutsideVehicle = ISVehicleMenu.FillMenuOutsideVehicle
+
 -- required for vehicles which are not using `Base` module --
 function rLib.ClientAuto.DebugReloadVehicleTextures(context, vehicle)
 	local option = rLib.ContextMenu.GetSubOption(context, "[DEBUG] Vehicle", "Reload Vehicle Textures")
@@ -14,8 +16,6 @@ function rLib.ClientAuto.DebugReloadVehicleTextures(context, vehicle)
 end
 
 if getDebug() then
-	local ISVM_FillMenuOutsideVehicle = ISVehicleMenu.FillMenuOutsideVehicle
-
 	function ISVehicleMenu.FillMenuOutsideVehicle(player, context, vehicle, ...)
 		ISVM_FillMenuOutsideVehicle(player, context, vehicle, ...)
 
