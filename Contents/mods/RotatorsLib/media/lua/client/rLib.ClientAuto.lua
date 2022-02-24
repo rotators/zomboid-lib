40feee454de0b40ab92ee6af0cc902b635e6ad39
Wipe <wipe.git@gmail.com>
2022-02-24 01:03:49 +0100

    AUTOFIX badly configured vehicles overlay (part config exists but `img` field is missing) rotators/zomboid-mods#6

--- Contents/mods/RotatorsLib/media/lua/client/rLib.ClientAuto.lua
+++ Contents/mods/RotatorsLib/media/lua/client/rLib.ClientAuto.lua
@@ -8,6 +8,18 @@ rLib.ClientAuto = {}
 
 local ISVM_FillMenuOutsideVehicle = ISVehicleMenu.FillMenuOutsideVehicle
 
+local function gTableSafeGet(...)
+	local var = _G
+	for _,val in ipairs({...}) do
+		if var[val] == nil then
+			return nil
+		end
+		var = var[val]
+	end
+
+	return var
+end
+
 -- required for vehicles which are not using `Base` module --
 function rLib.ClientAuto.DebugReloadVehicleTextures(context, vehicle)
 	local option = rLib.ContextMenu.GetSubOption(context, "[DEBUG] Vehicle", "Reload Vehicle Textures")
@@ -24,5 +36,37 @@ if getDebug() then
 		rLib.ClientAuto.DebugReloadVehicleTextures(context, vehicle)
 	end
 end
+
+-- required for vehicles which are badly configured --
+function rLib.ClientAuto.MechanicsOverlayMissingImg(ui, visible)
+	local vehicleName = ui.vehicle:getScript():getFullName()
+
+	for p=0, ui.vehicle:getPartCount()-1 do
+		local part = ui.vehicle:getPartByIndex(p)
+		local partId = part:getId()
+		local crappyConfig = "<<<RotatorsLib-ISCarMechanicsOverlay-BadlyConfiguredVehicle>>>"
+		if visible then
+			if ISCarMechanicsOverlay.PartList[partId] and ISCarMechanicsOverlay.PartList[partId].img == nil and gTableSafeGet("ISCarMechanicsOverlay", "CarList", vehicleName, "PartList", partId, "img") == nil then
+				rLib.dprint("[rLib] ISCarMechanicsOverlay texture hotfix add : %s", partId)
+				gTableSafeSet("ISCarMechanicsOverlay", "CarList", vehicleName, "PartList", partId, "img", crappyConfig)
+			end
+		else
+			if gTableSafeGet("ISCarMechanicsOverlay", "CarList", vehicleName, "PartList", partId, "img") == crappyConfig then
+				rLib.dprint("[rLib] ISCarMechanicsOverlay texture hotfix del : %s", partId)
+				ISCarMechanicsOverlay.CarList[vehicleName].PartList[partId].img = nil
+
+				if table.isempty(ISCarMechanicsOverlay.CarList[vehicleName].PartList[partId]) then
+					ISCarMechanicsOverlay.CarList[vehicleName].PartList[partId] = nil
+				end
+
+				if table.isempty(ISCarMechanicsOverlay.CarList[vehicleName].PartList) then
+					ISCarMechanicsOverlay.CarList[vehicleName].PartList = nil
+				end
+			end
+		end
+	end
+end
+
+rLib.Events.On("Vehicle.MechanicsSetVisible", rLib.ClientAuto.MechanicsOverlayMissingImg)
 
 return rLib.ClientAuto
